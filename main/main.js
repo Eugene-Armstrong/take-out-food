31f6594a28e7065249221f020f723d05433b3d6b
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-08 15:11:44 +0800

    [Jeffery] 1 Format the inputs and pass npm unit test

---
+++ main/main.js
@@ -0,0 +1,103 @@
+// Write your cade below:
+'use strict';
+
+const { loadAllItems } = require('../main/items');
+const { loadPromotions } = require('../main/promotions');
+
+const allItemsInfo = loadAllItems();
+const promotions = loadPromotions();
+
+function bestCharge(selectedItems) {
+    // 1 格式化输入
+    let formattedInputs = formatInputs(selectedItems);
+    // 2 整合获取已点各个菜品信息
+    // let eachItemInfo = getEachItemInfo(formatInputs, allItemsInfo);
+    // 3 计算已点菜品优惠前总价
+    // let originalSumPrice = calculateOriginalSumPrice(eachItemInfo);
+    // 4-1 计算第1种优惠下所有已点菜品总价
+    // let discSumPrice1 = calculateDiscSumPrice1(eachItemInfo,promotions);
+
+    // 4-2 计算第2种优惠下所有已点菜品总价
+
+    // 5 获取最佳优惠总价
+
+    // 6 返回最终汇总信息
+    console.info(formattedInputs);
+}
+
+/**
+ * 格式化输入 10 lines
+ * @param {输入信息} items 
+ */
+function formatInputs(items) {
+    let formattedInputs = [];
+    for (let item of items) {
+        let tmp = item.split(" x ");
+        formattedInputs.push({
+            id: tmp[0],
+            count: parseInt(tmp[1])
+        });
+    }
+    return formattedInputs;
+}
+
+module.exports = {
+    bestCharge,
+    formatInputs
+}
+
+/**
+ * 比对并获取已点菜品信息 12 lines
+ * @param {已点菜品信息} eachItemInfo 
+ * @param {所有优惠方式} promotions 
+ */
+function getEachItemInfo(eachItemInfo, promotions) {
+    let discSumPrice1 = 0;
+    for (let eachItemObj of eachItemInfo) {
+        for (let promotionId of promotions[1].items) {
+            if (eachItemObj.id === promotionId) {
+                discSumPrice1 += eachItemObj.count * (eachItemObj.price/2)
+            }
+        }
+    }
+    return discSumPrice1;
+}
+
+/**
+ * 
+ * @param {已点菜品信息} eachItemInfo 
+ */
+function calculateOriginalSumPrice(eachItemInfo) {
+    let originalSPrice = 0;
+    for (let eachItemObj of eachItemInfo) {
+        originalSPrice += eachItemObj.count * eachItemObj.price;
+    }
+    return originalSPrice;
+}
+
+/**
+ * 计算第1种优惠下所有菜品总价 12 lines
+ * @param {格式化输入} formatInputs 
+ * @param {所有菜品信息} allItemsInfo 
+ */
+function calculateDiscSumPrice1(formatInputs, allItemsInfo) {
+    let eachItemInfo = [];
+    for (let allItemsObj of allItemsInfo) {
+        for (let formatInputObj of formatInputs) {
+            if (formatInputObj.id === allItemsObj.id) {
+                const { id, name, price } = allItemsObj;
+                const { count } = formatInputObj;
+                eachItemInfo.push({ id, name, price, count });
+            }
+        }
+    }
+    return eachItemInfo;
+}
+
+module.exports = {
+    bestCharge,
+    formatInputs,
+    getEachItemInfo,
+    calculateOriginalSumPrice,
+    calculateDiscSumPrice1
+}
---
+++ spec/main-spec.js
@@ -0,0 +1,98 @@
+const {
+    bestCharge,
+    formatInputs,
+    getEachItemInfo,
+    calculateOriginalSumPrice,
+    calculateDiscSumPrice1
+} = require('../main/main');
+const {loadAllItems} = require('../main/items');
+const {loadPromotions} = require('../main/promotions');
+
+// describe('Take out food', function () {
+
+//   it('should generate best charge when best is 指定菜品半价', function () {
+//     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 黄焖鸡 x 1 = 18元
+// 肉夹馍 x 2 = 12元
+// 凉皮 x 1 = 8元
+// -----------------------------------
+// 使用优惠:
+// 指定菜品半价(黄焖鸡，凉皮)，省13元
+// -----------------------------------
+// 总计：25元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+//   it('should generate best charge when best is 满30减6元', function () {
+//     let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 肉夹馍 x 4 = 24元
+// 凉皮 x 1 = 8元
+// -----------------------------------
+// 使用优惠:
+// 满30减6元，省6元
+// -----------------------------------
+// 总计：26元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+//   it('should generate best charge when no promotion can be used', function () {
+//     let inputs = ["ITEM0013 x 4"];
+//     let summary = bestCharge(inputs).trim();
+//     let expected = `
+// ============= 订餐明细 =============
+// 肉夹馍 x 4 = 24元
+// -----------------------------------
+// 总计：24元
+// ===================================`.trim()
+//     expect(summary).toEqual(expected)
+//   });
+
+// });
+
+describe('Unit test', () => {
+    it('1 - Format the inputs', () => {
+        let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+        const actualResult = JSON.stringify(formatInputs(inputs));
+        const expectResult = JSON.stringify([
+            { id: 'ITEM0001', count: 1 },
+            { id: 'ITEM0013', count: 2 },
+            { id: 'ITEM0022', count: 1 },
+        ]);
+        expect(expectResult).toBe(actualResult);
+    });
+});
+
+// describe('Unit test', () => {
+//     it('2 - Compare and get the information of selected items', () => {
+//         let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+//         let formattedInputs = formatInputs(inputs);
+//         const allItemsInfo = loadAllItems(); // 所有菜品信息
+//         const actualResult = JSON.stringify(getEachItemInfo(formattedInputs, allItemsInfo));
+//         const expectResult = JSON.stringify([
+//             { id: 'ITEM0001', name: '黄焖鸡', price: 18.00, count: 1 },
+//             { id: 'ITEM0013', name: '肉夹馍', price: 6.00, count: 2 },
+//             { id: 'ITEM0022', name: '凉皮', price: 8.00, count: 1 },
+//         ]);
+//         expect(expectResult).toBe(actualResult);
+//     });
+// });
+
+// describe('Unit test', () => {
+//     it('3 - Calculate original sum price of the selected items', () => {
+//         let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+//         let formattedInputs = formatInputs(inputs);
+//         const allItemsInfo = loadAllItems(); // 所有菜品信息
+//         let eachItemInfo = getEachItemInfo(formattedInputs, allItemsInfo);
+//         const actualResult = calculateOriginalSumPrice(eachItemInfo);
+//         const expectResult = 38.00;
+//         expect(expectResult).toBe(actualResult);
+//     });
+// });
